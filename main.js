0fe587a722e663699d0b60258be2016a8f5c7a0f
Iliatsiklauri <tsiklauri.ilia26@gmail.com>
2023-12-02 20:23:41 +0400

    finished

--- main.js
+++ main.js
@@ -8,44 +8,59 @@ const button15 = document.querySelector(".box3");
 const button25 = document.querySelector(".box4");
 const button50 = document.querySelector(".box5");
 const buttonCustom = document.querySelector(".box6");
-const allButton = document.querySelectorAll(".box");
-
 const allButtons = document.querySelectorAll(".box");
-
-let isColorChanged = false;
-const defaultColors = {};
+const paragr = document.querySelector(".appear");
+const resetButn = document.querySelector(".reset_btn");
 allButtons.forEach((box) => {
-  defaultColors[box.textContent.trim()] = window.getComputedStyle(box).backgroundColor;
   box.addEventListener("click", () => {
-    Object.keys(defaultColors).forEach((buttonText) => {
-      if (buttonText !== box.textContent.trim()) {
-        allButtons.forEach((btn) => {
-          if (btn.textContent.trim() === buttonText) {
-            btn.style.backgroundColor = defaultColors[buttonText];
-          }
-        });
+    // Loop through all buttons to reset backgrounds except the clicked one
+    allButtons.forEach((btn) => {
+      if (btn !== box) {
+        btn.style.backgroundColor = ""; // Reset other buttons
       }
     });
 
-    if (box.textContent.trim() === "Custom") {
-      box.style.borderRadius = "5px";
-      box.style.outline = "2px solid #26c2ae";
-      box.style.transition = "outline-color 0.3s ease";
-    } else {
+    // Toggle the background color of the clicked box
+    if (box.style.backgroundColor !== "#26C2AE") {
       box.style.backgroundColor = "#26C2AE";
+    } else {
+      box.style.backgroundColor = ""; // Reset to original color
     }
   });
 });
 
 let buttonValue;
-allButton.forEach((box) => {
+
+allButtons.forEach((box) => {
   box.addEventListener("click", () => {
-    buttonValue = box.innerHTML.split("%")[0];
+    buttonValue = parseInt(box.innerHTML);
   });
 });
+
 inputPeople.addEventListener("input", () => {
-  personTotal.innerText = `$${
-    ((inputBill.value / 100) * (100 + buttonValue)) / inputPeople.value
-  }`;
-  console.log(((inputBill.value / 100) * (100 + buttonValue)) / inputPeople.value);
+  if (parseInt(inputPeople.value) === 0) {
+    paragr.classList.remove("none");
+  } else {
+    console.log(parseInt(inputBill.value) / 100);
+    console.log(100 + buttonValue);
+    console.log(parseInt(inputPeople.value));
+    personTotal.innerText = `$${
+      ((parseInt(inputBill.value) / 100) * (100 + buttonValue)) /
+      parseInt(inputPeople.value)
+    }`;
+    personTip.innerText = `$${
+      ((parseInt(inputBill.value) / 100) * buttonValue) / parseInt(inputPeople.value)
+    } `;
+    paragr.classList.add("none");
+  }
+});
+resetButn.addEventListener("click", () => {
+  inputBill.value = "";
+  inputPeople.value = "";
+  buttonValue.value = "";
+  personTip.innerText = "$0.00";
+  personTotal.innerText = "$0.00";
+  allButtons.forEach((box) => {
+    box.style.backgroundColor = "#00474B";
+  });
 });
